f42b5137e99fa6da2c267a937b1e53f571f771d7
kim kyeong min <kyeongmin21@naver.com>
2022-08-26 18:54:36 +0900

    useReducer 셋팅

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
+import {useReducer} from 'react';
+
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 import Home from './pages/Home';
 import Write from './pages/Write';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import MyHeader from "./components/MyHeader";
-import MyButton from "./components/MyButton";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch(action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CREATE' : {
+      const newItem = {
+        ...action.data
+      }
+      newState = [newItem, ...state];
+      break;
+    }
+    case 'REMOVE' : {
+      newState = state.filter((item) => item.id !== action.targetId)
+      break;
+    }
+    case 'EDIT' : {
+      newState = state.map((item) => item.id === action.data.id ? {...action.data} : item);
+      break;
+    }
+    default :
+      return state
+  }
+  return newState;
+}
 
 function App() {
-  // 이미지 설정
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const [data, dispatch] = useReducer(reducer, [])
 
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader headText={"App"}
-                  leftChild={
-                    <MyButton text={"왼쪽버튼"} onClick={() => alert('왼쪽 클릭')} />
-                  }
-                  rightChild={
-                    <MyButton text={"오른쪽버튼"} onClick={() => alert('오른쪽 클릭')}/>
-                  }
-        />
-
-        <MyButton text={'버튼'}
-                  onClick={() => alert('버튼클릭')}
-                  type={'positive'}/>
-        <MyButton text={'버튼'}
-                  onClick={() => alert('버튼클릭')}
-                  type={'negative'}/>
-        <MyButton text={'버튼'}
-                  onClick={() => alert('버튼클릭')}/>
-
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/write' element={<Write/>}/>
