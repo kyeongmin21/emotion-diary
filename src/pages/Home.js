d89f4974fb55cff36416ddd8a7db4ec944ddebcb
kim kyeong min <kyeongmin21@naver.com>
2022-08-28 15:18:12 +0900

    날짜 header 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
+import {useState} from 'react';
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const Home = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const headText = `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월`;
+
+  const inCreaseMonth = () => {
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, currentDate.getDate()))
+  }
+  const inDecreaseMonth = () => {
+    setCurrentDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate()))
+  }
+
   return (
     <div className="Home">
-      <h2>Home</h2>
+      <MyHeader
+        leftChild={ <MyButton text={"<"} onClick={inDecreaseMonth}/> }
+        headText={ headText }
+        rightChild={ <MyButton text={">"} onClick={inCreaseMonth}/> }
+        />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
