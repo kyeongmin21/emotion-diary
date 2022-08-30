0f8b917524b90dbd44c3daeb97fffd240e403944
kim kyeong min <kyeongmin21@naver.com>
2022-08-30 20:35:33 +0900

    정렬할 selectbox 생성

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,25 @@
+import {useState} from "react";
+
+const sortOptionList = [
+  {value: "latest", name: "최신순"},
+  {value: "oldest", name: "오래된 순"},
+]
+
+const ControlMenu = ({value, onChange, optionList}) => {
+  return <select value={value}
+                 onChange={(e) => e.target.value}>
+    {optionList.map((item, idx) => <option value={item.value} key={idx}>{item.name}</option>)}
+  </select>
+}
+
 const DiaryList = ({diaryList}) => {
+  // 정렬기준을 저장할 state
+  const [sortType, setSortType] = useState('latest');
   return (
     <div className="DiaryList">
+      <ControlMenu value={sortType}
+                   onChange={setSortType}
+                   optionList={sortOptionList}/>
       {diaryList.map((item) =>  (
         <div key={item.id}>
           {item.content}
@@ -9,5 +28,9 @@ const DiaryList = ({diaryList}) => {
     </div>
   );
 }
+
+DiaryList.defaultProps = {
+  diaryList: []
+};
 
 export default DiaryList
