c83d4e4197ffbde3e58003746469169fefd25dcf
kim kyeong min <kyeongmin21@naver.com>
2022-08-31 21:41:05 +0900

    selectbox 선택에 따른 정렬기능 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -6,21 +6,39 @@ const sortOptionList = [
 ]
 
 const ControlMenu = ({value, onChange, optionList}) => {
-  return <select value={value}
-                 onChange={(e) => e.target.value}>
-    {optionList.map((item, idx) => <option value={item.value} key={idx}>{item.name}</option>)}
-  </select>
+  return (
+    <select value={value}
+            onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((item, idx) => <option value={item.value} key={idx}>{item.name}</option>)}
+    </select>
+  )
 }
 
 const DiaryList = ({diaryList}) => {
   // 정렬기준을 저장할 state
   const [sortType, setSortType] = useState('latest');
+
+  const getProcessedDiaryList = () => {
+
+    // 객체로 이루어진 배열 : [ {}, {}, {} ]
+    // 그냥 정렬하면 정렬이 안됨. 그래서 비교함수를 만들어 줘야함
+    const compare = (a, b) => {
+      if (sortType === 'latest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    }
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    return copyList.sort(compare);
+  }
+
   return (
     <div className="DiaryList">
       <ControlMenu value={sortType}
                    onChange={setSortType}
                    optionList={sortOptionList}/>
-      {diaryList.map((item) =>  (
+      {getProcessedDiaryList().map((item) => (
         <div key={item.id}>
           {item.content}
         </div>
