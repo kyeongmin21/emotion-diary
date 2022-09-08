eb0f4acb772b1fb6c57c19481a1aadad4c508a67
kim kyeong min <kyeongmin21@naver.com>
2022-09-08 15:49:01 +0900

    DiaryItem 컴포넌트 분리, 이미지불러오기

---
+++ src/components/DiaryItem.js
@@ -0,0 +1,17 @@
+const DiaryItem = ({id, emotion, content, date}) => {
+  return (
+    <div className="DiaryItem">
+      <div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(' ')}>
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}  alt="감정이미지"/>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+};
+
+export default DiaryItem;
--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import MyButton from "./MyButton";
-
+import DiaryItem from "./DiaryItem"
 const sortOptionList = [
   {value: "latest", name: "최신순"},
   {value: "oldest", name: "오래된 순"},
@@ -72,7 +72,7 @@ const DiaryList = ({diaryList}) => {
 
 
       {getProcessedDiaryList().map((item) => (
-        <div key={item.id}>{item.content} {item.emotion}</div>
+       <DiaryItem key={item.id} {...item}/>
       ))}
     </div>
   );
