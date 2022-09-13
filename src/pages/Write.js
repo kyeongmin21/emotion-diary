265a46ac5e74d89f8a52dd423060d73011da9a64
kim kyeong min <kyeongmin21@naver.com>
2022-09-13 20:54:16 +0900

    일기쓰기의 header 부분 레이아웃 만들기

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,6 +1,11 @@
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
 const Write = () => {
   return (
     <div className="Write">
+      <MyHeader headText={"새 일기쓰기"}
+                leftChild={<MyButton text={"뒤로가기"} />} />
       <h2>Write</h2>
     </div>
   )
