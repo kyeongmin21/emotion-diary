0ac733556455ffaec41d4ef0fac152af8a75e2c7
kim kyeong min <kyeongmin21@naver.com>
2022-09-13 21:06:20 +0900

    뒤로가기 링크걸어주기

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,11 +1,14 @@
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
+import { useNavigator } from "react-router-dom";
 
 const Write = () => {
   return (
     <div className="Write">
       <MyHeader headText={"새 일기쓰기"}
-                leftChild={<MyButton text={"뒤로가기"} />} />
+                leftChild={<MyButton text={"뒤로가기"}
+                                     onClick={() => useNavigator(-1)} />}
+      />
       <h2>Write</h2>
     </div>
   )
