273eefafcc287690b37a3e2f1c8f8cf341004d33
kim kyeong min <kyeongmin21@naver.com>
2022-09-14 21:33:34 +0900

    Write 컴포넌트 안에 DiaryEditor 컴포넌트로 분리 시키기

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,35 +1,9 @@
-import {useNavigate} from "react-router-dom";
-import {useState} from "react";
-
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-}
+import DiaryEditor from "../components/DiaryEditor";
 
 const Write = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
-
   return (
     <div className="Write">
-      <MyHeader headText={"새 일기쓰기"}
-                leftChild={<MyButton text={"< 뒤로가기"}
-                                     onClick={() => navigate(-1)}/>}
-      />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
-            <input className="input-date"
-                   type="date"
-                   value={date}
-                   onChange={(e) => setDate(e.target.value)} />
-          </div>
-        </section>
-      </div>
-
+      <DiaryEditor/>
     </div>
   )
 }
