587d7212dc209e4c0dc2fb34dfa7b1ba62fd6f2f
kim kyeong min <kyeongmin21@naver.com>
2022-09-19 20:33:55 +0900

    감정아이디 props로 내려주기

--- src/components/EmotionItem.js
+++ src/components/EmotionItem.js
@@ -1,7 +1,7 @@
 
-const EmotionItem = ({emotion_id, emotion_img, emotion_description}) => {
+const EmotionItem = ({ emotion_id, emotion_img, emotion_description, onClick}) => {
   return (
-    <div className="emotionItem">
+    <div className="EmotionItem" onClick={() => onClick(emotion_id)}>
       <img src={emotion_img} alt="감정이미지"/>
       <span>{emotion_description}</span>
     </div>
