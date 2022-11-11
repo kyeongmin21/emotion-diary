cc630d54bdbe3b358969ca606361fe40df4a588e
kim kyeong min <kyeongmin21@naver.com>
2022-11-11 20:15:47 +0900

    페이지 라우팅1 - Router 기본

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import RouteTest from 'components/RouteTest';
+
+import Diary from 'pages/Diary';
+import Edit from 'pages/Edit';
+import Home from 'pages/Home';
+import Write from 'pages/Write';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App</h2>
+        <RouteTest/>
 
-    </div>
+        <Routes>
+          <Route path="/"      element={<Home/>}  />
+          <Route path="/write" element={<Write/>} />
+          <Route path="/edit"  element={<Edit/>}  />
+          <Route path="/diary" element={<Diary/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/RouteTest.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+
+const RouteTest = () => {
+  return (
+    <div className="RouteTest">
+      <div><Link to={'/'}>Home</Link></div>
+      <div><Link to={'/diary'}>Diary</Link></div>
+      <div><Link to={'/write'}>Write</Link></div>
+      <div><Link to={'/edit'}>Edit</Link></div>
+    </div>
+  )
+}
+
+export default RouteTest;
